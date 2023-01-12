e88004bd6958c5a876daa7c6f2b934c2d8f599d2
Keith Hall <keith.david.hall@vippsmobilepay.com>
2023-01-12 12:26:18 +0200

    really fix error with long querystrings

--- proxy.js
+++ proxy.js
@@ -77,6 +77,22 @@ function startNewRequest( cachedFile ){
   return stream;
 }
 
+// https://stackoverflow.com/a/52171480/4473405
+const cyrb53 = (str, seed = 0) => {
+  let h1 = 0xdeadbeef ^ seed,
+    h2 = 0x41c6ce57 ^ seed;
+  for (let i = 0, ch; i < str.length; i++) {
+    ch = str.charCodeAt(i);
+    h1 = Math.imul(h1 ^ ch, 2654435761);
+    h2 = Math.imul(h2 ^ ch, 1597334677);
+  }
+  
+  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+  
+  return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+}
+
 async function getContent(httpModule, origReq, origRes) {
   const origUrl = url.parse(origReq.url);
   const mappedUrlStr = mapUrl(origReq.url);
@@ -85,7 +101,7 @@ async function getContent(httpModule, origReq, origRes) {
   const method = origReq.method;
   const proto = httpModule === http ? 'http':'https';
 
-  const safe_filepath = mappedUrl.path.replace(/[\\?%*:|"<>&,]/g, '-'); // TODO: deal with relative paths going up further than they should?
+  const safe_filepath = mappedUrl.pathname + (mappedUrl.search ? cyrb53(mappedUrl.search) : ''); //mappedUrl.path.replace(/[\\?%*:|"<>&,]/g, '-'); // TODO: deal with relative paths going up further than they should?
   let cachedFile = `${config.cache_dir}/${proto}/${mappedUrl.host}${mappedPort}/${method}${safe_filepath}`;
   if( mappedUrl.path.slice(-1) === '/' ){
     cachedFile += '#index.data';
